8b4f46e634899121a219a11f679717fd5d88bdcf
Lukas Strnad <strnad.lukas@proton.me>
2023-08-21 23:17:38 +0200

    Adding a println! and print! macros

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,7 @@ mod vga_buffer;
 /// This is the entry point of our system
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        ", some test numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap();
+    println!("Hello World{}", "!");
 
     loop {}
 }
