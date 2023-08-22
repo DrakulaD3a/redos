47328404b8cc9e33d59b2daccfee668f2bc14cb8
Lukas Strnad <strnad.lukas@proton.me>
2023-08-22 14:02:59 +0200

    Adding basic integration test

---
+++ tests/basic_boot.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rudos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+/// Use the rudos' test runner
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    rudos::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    rudos::println!("test_println output");
+}
