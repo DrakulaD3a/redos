8eee4103a85cd0ce6c33b9d0e979738c2acbce31
Lukas Strnad <strnad.lukas@proton.me>
2023-08-22 09:14:26 +0200

    Adding a separate panic for tests

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,20 @@ pub extern "C" fn _start() -> ! {
 }
 
 /// This is the panic handler
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     println!("{info}");
+    loop {}
+}
+
+/// This is the panic handler for tests
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
@@ -41,7 +52,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
 #[test_case]
 fn basic_test() {
     serial_print!("trivial assertion... ");
-    assert_eq!(1, 1);
+    assert_eq!(1, 2);
     serial_println!("[ok]");
 }
 
