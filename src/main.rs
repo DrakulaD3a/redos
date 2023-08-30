dfa7b2fba3b723af0f28c89765c0aba41e14c146
Lukas Strnad <strnad.lukas@proton.me>
2023-08-30 05:48:29 +0200

    Using the x86_64 crates implementation of address translation instead of my own

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,14 @@ entry_point!(kernel_main);
 
 /// This is the typechecked entry point of our system
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use rudos::memory::translate_addr;
-    use x86_64::VirtAddr;
+    use rudos::memory;
+    use x86_64::{structures::paging::Translate, VirtAddr};
 
     println!("Hello World{}", "!");
     rudos::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
     let addresses = [
         0xb8000,
         0x201008,
@@ -27,7 +28,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     for &address in &addresses {
         let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        let phys = mapper.translate_addr(virt);
         println!("{:?} -> {:?}", virt, phys);
     }
 
--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,21 @@
-use x86_64::{structures::paging::PageTable, PhysAddr, VirtAddr};
+use x86_64::{
+    structures::paging::{OffsetPageTable, PageTable},
+    VirtAddr,
+};
+
+/// Initializes a new `OffsetPageTable`.
+///
+/// # Safety
+///
+/// This function is unsafe because the caller must guarantee that
+/// the complete physical memory is mapped to virtual memory at
+/// the `physical_memory_offset`. This function can only be called
+/// once to avoid aliasing &mut references (which results in UB).
+#[must_use]
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level4_table = active_level4_table(physical_memory_offset);
+    OffsetPageTable::new(level4_table, physical_memory_offset)
+}
 
 /// Returns a mutable reference to the active level 4 table.
 ///
@@ -9,7 +26,7 @@ use x86_64::{structures::paging::PageTable, PhysAddr, VirtAddr};
 /// function can only be called once to avoid aliasing
 /// &mut references (which results in UB).
 #[must_use]
-pub unsafe fn active_level4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+unsafe fn active_level4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     use x86_64::registers::control::Cr3;
 
     let (level4_table_frame, _) = Cr3::read();
@@ -20,49 +37,4 @@ pub unsafe fn active_level4_table(physical_memory_offset: VirtAddr) -> &'static 
 
     // unsafe
     &mut *page_table_ptr
-}
-
-/// Translate given virtual address to physical address or `None`
-/// if the address is not mapped.
-///
-/// # Safety
-///
-/// This function is unsafe because the caller must guarantee that
-/// the complete physical memory is mapped to virtual memory at
-/// the `physical_memory_offset`. This function can only be called
-/// once to avoid aliasing &mut references (which results in UB).
-///
-/// # Panics
-///
-/// This function panics when encountering huge page.
-#[must_use]
-pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
-    use x86_64::{registers::control::Cr3, structures::paging::page_table::FrameError};
-
-    // Read the active level4 from the Cr3 register
-    let (level4_table_frame, _) = Cr3::read();
-
-    let table_indices = [
-        addr.p4_index(),
-        addr.p3_index(),
-        addr.p2_index(),
-        addr.p1_index(),
-    ];
-    let mut frame = level4_table_frame;
-
-    // Traverse the multilevel page table
-    for &index in &table_indices {
-        let virt = physical_memory_offset + frame.start_address().as_u64();
-        let table_ptr: *const PageTable = virt.as_ptr();
-        let table = unsafe { &*table_ptr };
-
-        let entry = &table[index];
-        frame = match entry.frame() {
-            Ok(frame) => frame,
-            Err(FrameError::FrameNotPresent) => return None,
-            Err(FrameError::HugeFrame) => panic!("Huge pages not supported!"),
-        };
-    }
-
-    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
